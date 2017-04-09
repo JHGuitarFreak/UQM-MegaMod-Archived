fa51768503b3030e4046b650651d48a882cf0012
CTHULHU\seros <serosis@gmail.com>
2017-04-08 19:31:16 -0500

    Remove the Ur-Quan Probe for Head Start

--- HD/src/uqm/planets/generate/gensol.c
+++ HD/src/uqm/planets/generate/gensol.c
@@ -28,6 +28,7 @@
 #include "../../nameref.h"
 #include "../../state.h"
 #include "libs/mathlib.h"
+#include "options.h"
 
 #include <math.h>
 
@@ -80,7 +81,7 @@ GenerateSol_initNpcs (SOLARSYS_STATE *solarSys)
 		SET_GAME_STATE_32 (URQUAN_PROBE_GRPOFFS0, GLOBAL (BattleGroupRef));
 	}
 
-	if (!init_probe ())
+	if (!init_probe () || optHeadStart)
 		GenerateDefault_initNpcs (solarSys);
 
 	return true;
