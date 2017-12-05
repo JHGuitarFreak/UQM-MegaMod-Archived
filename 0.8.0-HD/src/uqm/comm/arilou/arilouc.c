d35c428e3eca42a331944586b8489bd1e81d59c8
K2ULU\seros <serosis@gmail.com>
2017-12-05 15:19:19 -0800

    Some cleanup

--- 0.8.0-HD/src/uqm/comm/arilou/arilouc.c
+++ 0.8.0-HD/src/uqm/comm/arilou/arilouc.c
@@ -502,9 +502,7 @@ AngryHomeArilou (RESPONSE_REF R)
 	else if (PHRASE_ENABLED (what_about_interference))
 		Response (what_about_interference, AngryHomeArilou);
 	Response (ok_lets_be_friends, ArilouHome);
-	if (PHRASE_ENABLED (i_just_like_to_leave)){
-		Response (i_just_like_to_leave, AngryHomeArilou);
-	}
+	Response (i_just_like_to_leave, AngryHomeArilou);
 }
 
 static void
