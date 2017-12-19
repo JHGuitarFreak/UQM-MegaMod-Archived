51e6b1b4fdfcd70e6d855f47d65c5ae7d9f3698e
K2ULU\seros <serosis@gmail.com>
2017-12-19 02:48:25 -0800

    Draw a nebula over the starmap.

--- 0.8.0-HD/src/uqm/planets/pstarmap.c
+++ 0.8.0-HD/src/uqm/planets/pstarmap.c
@@ -697,7 +697,15 @@ DrawStarMap (COUNT race_update, RECT *pClipRect)
 
 		++SDPtr;
 	} while (SDPtr->star_pt.x <= MAX_X_UNIVERSE
-			&& SDPtr->star_pt.y <= MAX_Y_UNIVERSE);
+			&& SDPtr->star_pt.y <= MAX_Y_UNIVERSE);	
+	
+	// JMS: Draw a sexy nebula on the map's background (Only in Hyperspace, not in Quasispace).
+	if (which_space <= 1)
+	{
+		nebula.origin.x = nebula.origin.y = 0;
+		nebula.frame = SetAbsFrameIndex (NebulaeFrame, 16);
+		DrawStamp(&nebula);
+	}
 
 	// JMS: This draws the constellation lines on the constellation starmap.
 	if (which_space <= 1 && which_starmap == CONSTELLATION_STARMAP)
@@ -759,15 +767,6 @@ DrawStarMap (COUNT race_update, RECT *pClipRect)
 			DrawCursor (UNIVERSE_TO_DISPX (cursorLoc.x),
 					UNIVERSE_TO_DISPY (cursorLoc.y));
 		}
-	}
-	
-	
-	// JMS: Draw a sexy nebula on the map's background (Only in Hyperspace, not in QS/Orzspace!).
-	if (which_space <= 1)
-	{
-		nebula.origin.x = nebula.origin.y = 0;
-		nebula.frame = SetAbsFrameIndex (NebulaeFrame, 16);
-		DrawStamp(&nebula);
 	}
 }
 
