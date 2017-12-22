c1dc2e5c06bf113d2dcb4e0a804e6c94865f3748
K2ULU\seros <serosis@gmail.com>
2017-12-22 04:07:30 -0800

    no message

--- 0.8.0-HD/src/uqm/planets/cargo.c
+++ 0.8.0-HD/src/uqm/planets/cargo.c
@@ -195,7 +195,7 @@ DrawCargoDisplay (void)
 	for (i = 0; i < NUM_ELEMENT_CATEGORIES; ++i, cy += ELEMENT_SPACING_Y)
 	{
 		// erase background under an element icon
-		SetContextForeGroundColor (CARGO_BACK_COLOR);
+		SetContextForeGroundColor (CARGO_BACK_COLOR); // Serosis: Was black for some reason.
 		r.corner.y = cy;
 		DrawFilledRectangle (&r);
 
@@ -208,7 +208,7 @@ DrawCargoDisplay (void)
 	}
 
 	// erase background under the Bio icon
-	SetContextForeGroundColor (BLACK_COLOR);
+	SetContextForeGroundColor (CARGO_BACK_COLOR);
 	r.corner.y = BIO_ORG_Y;
 	DrawFilledRectangle (&r);
 
