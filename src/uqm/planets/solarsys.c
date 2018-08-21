3d48e07bbf551d4b4d318f38e950dfcbaf98c2aa
Unknown <serosis@gmail.com>
2018-08-21 08:16:33 -0700

    Changed this back because it also screwed with orbit view.

--- src/uqm/planets/solarsys.c
+++ src/uqm/planets/solarsys.c
@@ -1111,7 +1111,6 @@ ValidateOrbit (PLANET_DESC *planet, int sizeNumer, int dyNumer, int denom)
 			case 5: planet->size = 29;
 				break;
 			}
-			planet->size = (optScalePlanets ? RES_SCALE(planet->size) : planet->size);
 		}
 		planet->image.frame =	SetAbsFrameIndex (OrbitalFrame,
 				(Size << FACING_SHIFT) + NORMALIZE_FACING (
@@ -1630,7 +1629,10 @@ DrawTexturedBody (PLANET_DESC* planet, STAMP s)
 	
 	BatchGraphics ();
 	oldMode = SetGraphicScaleMode (TFB_SCALE_BILINEAR);
-	oldScale = SetGraphicScale (GSCALE_IDENTITY * planet->size / (worldIsMoon(pSolarSysState, planet) ? GENERATE_MOON_DIAMETER : GENERATE_PLANET_DIAMETER));
+	if (worldIsMoon(pSolarSysState, planet)) 
+		oldScale = SetGraphicScale (GSCALE_IDENTITY * (optScalePlanets ? RES_SCALE(planet->size) : planet->size) / GENERATE_MOON_DIAMETER); 
+	else
+		oldScale = SetGraphicScale (GSCALE_IDENTITY * (optScalePlanets ? RES_SCALE(planet->size) : planet->size) / GENERATE_PLANET_DIAMETER); 
 	s.frame = planet->orbit.SphereFrame;
 	DrawStamp (&s);
 	if (planet->orbit.ObjectFrame) {
