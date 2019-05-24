dc0a42eb980a79bcc4a8fc0512325fd2f62df354
Serosis <serosis@gmail.com>
2019-05-23 18:47:12 -0700

    Second part of the Syreen SoI code
    Commented out until I figure out how I want to implement it.

--- src/uqm/gameev.c
+++ src/uqm/gameev.c
@@ -807,7 +807,7 @@ advance_mycon_mission (int arg)
 			// Head back.
 			SET_GAME_STATE (MYCON_KNOW_AMBUSH, 1);
 			SetRaceDest (MYCON_SHIP, 6392, 2200, 30, (BYTE)~0);
-
+			//SetRaceDest (SYREEN_SHIP, 4125, 3770, 15, (BYTE)~0);
 			MyconPtr->growth = 0;
 			MyconPtr->growth_fract = 0;
 		}
