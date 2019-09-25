991c9fdf87e94bcff0a80ff8922b4f90d8871d97
Ala-lala <>
2019-09-25 15:57:53 -0700

    Fix issues with the Sa-Matra and Druuge AIs' use of WORLD_TO_TURN in HD

--- src/uqm/ships/lastbat/lastbat.c
+++ src/uqm/ships/lastbat/lastbat.c
@@ -563,10 +563,10 @@ sentinel_preprocess (ELEMENT *ElementPtr)
 					- (SDWORD)ElementPtr->current.location.y;
 			delta_y = WRAP_DELTA_Y (delta_y);
 
-			if ((num_frames = WORLD_TO_TURN (
+			if ((num_frames = RES_DESCALE (WORLD_TO_TURN (
 					square_root ((long)delta_x * delta_x
 					+ (long)delta_y * delta_y)
-					)) == 0)
+					))) == 0)
 				num_frames = 1;
 
 			TargetVelocity = TargetPtr->velocity;
